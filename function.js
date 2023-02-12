b6b531eed3cd7f704fb8157988d5cf3bc53c0753
Daniel Simoni Wegher <w.daniels@hotmail.com>
2023-02-12 16:27:35 -0300

    first commit

---
+++ function.js
@@ -0,0 +1,52 @@
+const user = {
+    username: "",
+    password: ""
+}
+const usuarioCorreto = {
+    nameadd: "Daniel",
+    senha: "1234"
+};
+
+
+// let login = document.getElementById('login');
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+
+
+const handleChangeUserName = (e) => {user.username=e.target.value}
+const handleChangePassword = (e) => {user.password=e.target.value}
+
+// login.addEventListener('click', clicou)
+username.addEventListener('change', handleChangeUserName )
+password.addEventListener('change', handleChangePassword)
+
+const form = document.querySelector('#form')
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    clicou();
+})
+
+function clicou () {
+
+    const db = JSON.parse(localStorage.getItem('db'))
+    
+    
+if (user.username == db.nameadd && user.password == db.senha) {
+
+        alert("Você está logado!")
+        location.href = './loginin.html';
+   
+    }else {
+        alert("Credenciais incorretas")
+    }
+};
+
+
+
+
+window.onload = () => {
+    localStorage.setItem('db',JSON.stringify(usuarioCorreto))
+}
+
+
